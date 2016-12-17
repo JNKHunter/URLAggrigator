8d9016e61e64c0f9f0357c943e8ff1537ce00dc0
John Hunter <john@push-it-forward.com>
2016-12-17 15:09:37 -0800

    main and mapper method

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by jhunter on 12/17/16.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
---
+++ src/main/java/MaxTempMapper.java
@@ -0,0 +1,5 @@
+/**
+ * Created by jhunter on 12/17/16.
+ */
+public class MaxTempMapper {
+}
