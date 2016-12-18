41979ba19f4064e200584f7b594177d40e76358f
John Hunter <john@push-it-forward.com>
2016-12-17 18:31:46 -0800

    added map

--- src/main/java/UrlMapper.java
+++ src/main/java/UrlMapper.java
@@ -8,14 +8,13 @@ import java.io.IOException;
 /**
  * Created by jhunter on 12/17/16.
  */
-public class MaxTempMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+public class UrlMapper extends Mapper<Text, IntWritable, Text, IntWritable> {
 
     private final IntWritable one = new IntWritable(1);
 
 
     @Override
-    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        String line = value.toString();
-        String year = line.substring(0,4);
+    protected void map(Text key, IntWritable value, Context context) throws IOException, InterruptedException {
+        context.write(new Text(key), one);
     }
 }
