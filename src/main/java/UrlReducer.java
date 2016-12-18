bb232d7c248799f0ed36727525d89027d2a36664
John Hunter <john@push-it-forward.com>
2016-12-17 18:37:11 -0800

    added reduce method

--- src/main/java/UrlReducer.java
+++ src/main/java/UrlReducer.java
@@ -2,13 +2,21 @@ import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Reducer;
 
+import java.io.IOException;
+
 /**
  * Created by jhunter on 12/17/16.
  */
 public class UrlReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
 
     @Override
-    public void reduce(Text key, Iterable<IntWritable> values, Context context){
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException{
+        int sum = 0;
 
+        for(IntWritable value : values){
+            sum += 1;
+        }
+
+        context.write(key, new IntWritable(sum));
     }
 }
