bbe7fddc180ae4c4906161cf8f5965d29f08367c
John Hunter <john@push-it-forward.com>
2016-12-17 18:45:04 -0800

    job config

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,38 @@
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+
 /**
  * Created by jhunter on 12/17/16.
  */
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello World");
+    public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+        if(args.length != 2){
+            System.err.println("Must provide an input and output dir");
+            System.exit(-1);
+
+        }
+
+        Job job = new Job();
+        job.setJarByClass(Main.class);
+        job.setJobName("URL counter");
+
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+        job.setMapperClass(UrlMapper.class);
+        job.setReducerClass(UrlReducer.class);
+
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 
 }
